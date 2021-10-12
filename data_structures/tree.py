fee90fe465ea3847a7e7e25ca96f4ac854315130
Jainesh J Hira <jaineshster@gmail.com>
2021-10-12 20:54:57 +0200

    Some Trees and insertion sort

--- data_structures/tree.py
+++ data_structures/tree.py
@@ -32,8 +32,51 @@ class Tree:
             else:
                 self._add(data, node.left)
 
+    def contains(self, data):
+
+        if self._root:
+            response = self._contains(data, self._root)
+            if response:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def _contains(self, data, node):
+
+        if not node:
+            return False
+        elif data == node.data:
+            return True
+        elif data > node.data:
+            return self._contains(data, node.right)
+        else:
+            return self._contains(data, node.left)
+
     # def print_tree(self):
     #     # Level order traversal
+
+    def remove(self, data):
+
+        self._remove(data, self._root)
+
+    def _remove(self, data, node):
+
+        if node is None:
+            return node
+
+        if data < node.data:
+            node.left = self._remove(data, node.left)
+        elif data > node.data:
+            node.right = self._remove(data, node.right)
+        else:
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+
+            
 
 
 temp_tree = Tree()
@@ -42,5 +85,7 @@ temp_tree.add(6)
 temp_tree.add(15)
 temp_tree.add(4)
 temp_tree.add(7)
+
+print(temp_tree.contains(7))
 
 print("Done")
---
+++ sorts/insertion_sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(data: list):
+
+    for i in range(1, len(data)):
+        value_to_compare = data[i]
+
+        for j in range(i-1, -1, -1):
+
+            if data[j] < value_to_compare:
+                break
+            else:
+                data[j+1] = data[j]
+            data[j] = value_to_compare
+
+    return data
+
+
+print(insertion_sort([7, 8, 6, 3, 9]))

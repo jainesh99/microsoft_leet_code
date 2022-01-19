05537fcc3ddb88c1e83df06b34e519ed8bca5087
Jainesh J Hira <jaineshster@gmail.com>
2022-01-19 16:17:54 +0200

    Added reverse str and critical_connections

---
+++ questions/critical_connections.py
@@ -0,0 +1,52 @@
+from typing import List
+import collections
+
+
+class Solution:
+    def criticalConnections(
+        self, n: int, connections: List[List[int]]
+    ) -> List[List[int]]:
+        graph = collections.defaultdict(list)
+
+        for i, j in connections:
+            graph[i].append(j)
+            graph[j].append(i)
+
+        low = [0] * n
+
+        visited = set()
+
+        res = []
+
+        def dfs(node, parent, timer):
+
+            nonlocal low, graph, res, visited
+
+            low[node] = timer
+
+            visited.add(node)
+
+            for child in graph[node]:
+
+                if child == parent:
+                    continue
+                if child not in visited:
+                    dfs(child, node, timer + 1)  # Probably can be done with a stack!
+
+                low[node] = min(low[node], low[child])
+
+                # important condition
+
+                # this will check all the node which are reachable from the this node having time greater than the parent if less then it will be access from somewhere else
+                # in teh minimum time
+
+                if timer < low[child]:
+                    res.append([node, child])
+
+        dfs(0, -1, 0)
+        return res
+
+
+solution = Solution()
+print(solution.criticalConnections(4, [[0, 1], [1, 2], [2, 0], [1, 3]]))
+# print(solution.criticalConnections(6, [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]]))
---
+++ questions/reverse_str.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+
+        if len(s) < k:
+            return s[::-1]
+
+        if len(s) < (2 * k) and len(s) >= k:
+            first_chars = s[0:k]
+            return first_chars[::-1] + s[k:]
+
+        return_str = ""
+
+        for i in range(0, len(s), 2 * k):
+            word_to_reverse = s[i : i + k]
+            return_str = return_str + word_to_reverse[::-1] + s[i + k : i + 2 * k]
+
+        return return_str
+
+
+solution = Solution()
+print(solution.reverseStr("abcdefghijklmno", 3))

c4991f7f093267a382f81d32c12db5682ab67715
Jainesh J Hira <jaineshster@gmail.com>
2022-01-20 19:34:52 +0200

    Added the connect sticks

---
+++ questions/connect_sticks.py
@@ -0,0 +1,28 @@
+from typing import List
+from heapq import heapify, heappop, heappush
+
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        cost = 0
+
+        # while len(sticks) > 1:
+        #     sticks = sorted(sticks)
+        #     first_val = sticks.pop(0)
+        #     second_val = sticks.pop(0)
+        #     cost = cost + first_val + second_val
+        #     sticks.append(first_val + second_val)
+
+        # Heap
+        heapify(sticks)
+
+        while len(sticks) > 1:
+            value_to_add = heappop(sticks) + heappop(sticks)
+            cost = cost + value_to_add
+            heappush(sticks, value_to_add)
+
+        return cost
+
+
+solution = Solution()
+sticks = [2,4,3]
+print(solution.connectSticks(sticks))

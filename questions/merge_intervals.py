6da32506f62a38e269a25f87fe3bffe763514bc4
Jainesh J Hira <jaineshster@gmail.com>
2022-01-22 11:58:29 +0200

    Updated merge intervals

--- questions/merge_intervals.py
+++ questions/merge_intervals.py
@@ -3,20 +3,23 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
 
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort()
+        result = [intervals[0]]
 
-        merged = []
-        for interval in intervals:
-            # merged[-1] means the last value!
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
+        for start, end in intervals[1:]:
+            prev_start = result[-1][0]
+            prev_end = result[-1][1]
+            if (start <= prev_end) and (start >= prev_start):
+                result[-1][1] = max(prev_end, end)
             else:
-                merged[-1][1] = max(merged[-1][1], interval[1])
+                result.append([start, end])
 
-        return merged
+        return result
 
 
 solution = Solution()
-intervals = [[5, 10], [5, 11], [8, 10], [15, 18]]
+intervals = [[9, 10], [10, 11], [8, 10], [15, 18]]
 solution.merge(intervals)

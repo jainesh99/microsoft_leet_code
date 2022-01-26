9855b8b6c22459e49573646b5c5185510d3e738c
Jainesh J Hira <jaineshster@gmail.com>
2022-01-26 14:17:12 +0200

    Added more questions

---
+++ questions/count_characters.py
@@ -0,0 +1,43 @@
+from typing import List
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+
+        def generate_dict(word: str):
+            result_dictionary = {}
+
+            for char in word:
+
+                if char in result_dictionary.keys():
+                    result_dictionary[char] += 1
+                else:
+                    result_dictionary[char] = 1
+
+            return result_dictionary
+
+        char_dict = generate_dict(chars)
+        counter = 0
+        for word in words:
+
+            word_dict = generate_dict(word)
+            word_list = [char for char in word]
+            for key in word_dict.keys():
+                if key in char_dict.keys():
+
+                    if word_dict[key] <= char_dict[key]:
+                        for i in range(0, word_dict[key]):
+                            word_list.remove(key)
+                else:
+                    break
+
+            if len(word_list) == 0:
+                counter = counter + len(word)
+
+        return counter
+
+
+
+solution = Solution()
+words = ["hello","world","leetcode"]
+chars = "welldonehoneyr"
+print(solution.countCharacters(words, chars))
---
+++ questions/num_rolls_target.py
@@ -0,0 +1,25 @@
+# exmaple
+# target = 5, d(dice) = 2, k(faces) = 2
+# .    0   1   2   3   4   5
+# d0 | 1 | 0 | 0 | 0 | 0 | 0 |
+# d1 | 0 | 1 | 1 | 0 | 0 | 0 |
+# d2 | 0 | 0 | 1 | 2 | 1 | 0 |
+# d3 | 0 | 0 | 0 | 1 | 3 | 3 |
+
+# each value in the table is equal to the all of the combinations of the previous row summed where you can add a dice value and get the new sum
+
+# e.g. you can make 5 with 3 2-sided dice with [2][3] 3(2 ways) + 2 and [2][4] 4(1 way) + 1 or 3 total ways. All values for the previous dice less than target - max face value (or 5 - 3 in this exmaple) can be ignored since it is impossible to produce the target with these values
+
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        table = [[0] * (target + 1) for _ in range(n + 1)]
+        # can make none with 0 dice 1 way as base case
+        table[0][0] = 1
+
+        for dice in range(1, len(table)):
+            # can ignore all targets that are less than the number of currently available dice which is why we can start with 'dice'
+            for ways in range(dice, len(table[0])):
+                table[dice][ways] = sum(
+                    [table[dice - 1][validPrevDiceWays] for validPrevDiceWays in range(max(ways - k, 0), ways)])
+
+        return table[-1][-1] % (10 ** 9 + 7)

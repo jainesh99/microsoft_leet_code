5ada6bf351f6099256185c2ac3ecd2791a729311
Jainesh J Hira <jaineshster@gmail.com>
2022-08-01 18:39:54 -0700

    Added more dynamic programming

---
+++ dynamic_programming/best_sum.py
@@ -0,0 +1,38 @@
+from typing import List, Dict
+
+
+def best_sum(target_sum: int, numbers: List[int], memo: [Dict] = None) -> List[int]:
+
+    if memo is None:
+        memo = {}
+
+    if target_sum in memo.keys():
+        return memo[target_sum]
+
+    shortest_sum = None
+
+    if target_sum == 0:
+        return []
+
+    if target_sum < 0:
+        return None
+
+    for number in numbers:
+
+        sum_arr = best_sum(target_sum - number, numbers, memo)
+
+        if sum_arr is not None:
+
+            sum_arr.append(number)
+
+            if shortest_sum is None or len(sum_arr) < len(shortest_sum):
+                shortest_sum = sum_arr.copy()
+
+    memo[target_sum] = shortest_sum
+
+    return shortest_sum
+
+
+print(best_sum(8, [2, 3, 5]))
+print(best_sum(7, [5, 3, 4, 7]))
+print(best_sum(100, [25, 1, 2, 5]))
---
+++ dynamic_programming/how_sum.py
@@ -0,0 +1,36 @@
+from typing import List, Dict
+
+
+def how_sum(targetSum: int, numbers: List[int], memo: Dict = None):
+
+    if memo is None:
+        memo = {}
+
+    if targetSum in memo.keys():
+        return memo[targetSum]
+
+    if targetSum == 0:
+        return []
+
+    if targetSum < 0:
+        return None
+
+    for number in numbers:
+
+        return_arr = how_sum(targetSum - number, numbers, memo)
+
+        if return_arr is not None:
+
+            return_arr.append(number)
+            memo[targetSum] = return_arr
+            return memo[targetSum]
+
+    memo[targetSum] = None
+    return None
+
+
+print(how_sum(7, [2, 3]))
+print(how_sum(7, [5, 3, 4, 7]))
+print(how_sum(7, [2, 4]))
+print(how_sum(8, [2, 3, 5]))
+print(how_sum(300, [7, 14]))

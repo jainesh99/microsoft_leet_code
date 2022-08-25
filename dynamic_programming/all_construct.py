487d3592ff7b2c2539f7da8d7bc587fd6daded45
Jainesh J Hira <jaineshster@gmail.com>
2022-08-24 18:57:05 -0700

    Added all construct

---
+++ dynamic_programming/all_construct.py
@@ -0,0 +1,33 @@
+from typing import List, Dict
+
+
+def all_construct(target: str, word_bank: List[str], memo: Dict = None):
+
+    if memo is None:
+        memo = {}
+
+    if target in memo.keys():
+        return memo[target]
+
+    if target == "":
+        return [[]]
+
+    number_of_ways = []
+
+    for word in word_bank:
+
+        if target.startswith(word):
+
+            value = all_construct(target.removeprefix(word), word_bank)
+
+            if len(value) > 0:
+                for item in value:
+                    item.insert(0, word)
+                    number_of_ways.append(item)
+
+    memo[target] = number_of_ways
+    return number_of_ways
+
+
+print(all_construct("purple", ["purp", "p", "ur", "le", "purpl"]))
+print(all_construct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))

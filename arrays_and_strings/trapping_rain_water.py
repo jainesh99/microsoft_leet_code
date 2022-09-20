4af139265a4c6f3df6980dedbdadd07fd330646a
Jainesh J Hira <jaineshster@gmail.com>
2022-09-19 17:56:28 -0700

    Added trapping rain water

---
+++ arrays_and_strings/trapping_rain_water.py
@@ -0,0 +1,36 @@
+from typing import List, NamedTuple
+
+
+class IndexHeight(NamedTuple):
+    index: int
+    height: int
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        stack = []
+        water = 0
+        for index, bar_height in enumerate(height):
+            # we need to see if we can form a container
+            while stack and bar_height >= stack[-1][0]:
+                popped, _ = stack.pop()
+                # is it a container though? we have a left border?
+                if stack:
+                    left_border, j = stack[-1]
+                    # we compute the water
+                    water += min(left_border - popped, bar_height - popped) * (
+                        index - j - 1
+                    )
+            stack.append((bar_height, index))
+        return water
+
+
+solution = Solution()
+solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])
+
+"""
+Basically you are adding the items to the stack, once you find an item which is greater than the top of the stack
+You the pop it, and by doing so you skip an element to go further back where the actual left border is
+From there you calculate how much is left
+"""

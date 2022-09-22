e0e03cf055970e51153ca8e571aafda2a41ea3a9
Jainesh J Hira <jaineshster@gmail.com>
2022-09-21 17:17:49 -0700

    Added rotate

---
+++ arrays_and_strings/rotate.py
@@ -0,0 +1,32 @@
+from typing import List, NamedTuple, Tuple
+
+
+class Move(NamedTuple):
+
+    value: int
+    position: Tuple[int, int]
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+        n = len(matrix)
+        moving_stack = []
+
+        for i in range(n):
+
+            end = n - i - 1
+
+            for j in range(n):
+
+                moving_stack.append(Move(value=matrix[i][j], position=(j, end)))
+
+        for item in moving_stack:
+
+            matrix[item.position[0]][item.position[1]] = item.value
+
+
+solution = Solution()
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+solution.rotate(matrix)
+print(matrix)

0f5e8472102895f8fc2ba3c63bfc6a25711bcc7b
Jainesh J Hira <jaineshster@gmail.com>
2023-04-12 19:37:44 -0700

    Added sort by frequency

---
+++ questions/1636_sort_array_by_frequency.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+
+        frequency_map = defaultdict(int)
+
+        for num in nums:
+            frequency_map[num] += 1
+
+        return sorted(nums, key=lambda x: (frequency_map[x], -x))
+
+
+solution = Solution()
+nums = [2, 3, 1, 3, 2]
+print(solution.frequencySort(nums))

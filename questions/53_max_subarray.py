c855c7783e2a1e8e01c8a5c2cb29383542a3d2ef
Jainesh J Hira <jaineshster@gmail.com>
2023-09-20 20:47:03 -0700

    Adding more questions

---
+++ questions/53_max_subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        max_array = nums[0]
+        current_sum = 0
+
+        for num in nums:
+
+            if current_sum < 0:
+                current_sum = 0
+
+            current_sum += num
+            max_array = max(current_sum, max_array)
+
+        return max_array
